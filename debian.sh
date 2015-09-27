269dbff4ac85b47c70561e6d78fa973823e35139
Changli Gao <xiaosuo@gmail.com>
2015-09-27 15:22:24 +0800

    Add debian.sh for Debian 8

---
+++ debian.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+if [[ x`lsb_release -is` != xDebian ]]; then
+	echo "Only Debian is supported"
+	exit 1
+fi
+
+rel=`lsb_release -rs | cut -d. -f1`
+if [[ $rel -ne 8 ]]; then
+	echo "Only Debian 8 is tested"
+	exit 1
+fi
+
+apt-get install build-essential vim-nox git python-dev cmake libclang-dev \
+	ruby ri ctags cscope
+git submodule update --init --recursive
+cd src/.vim/bundle/YouCompleteMe
+./install.sh --clang-completer --system-libclang
